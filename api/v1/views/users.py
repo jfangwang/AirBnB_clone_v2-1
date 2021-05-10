dc217cf186b6acf82ff07f3a606bab332aded692
jfangwang <qbs1864@gmail.com>
2021-05-10 19:46:22 +0000

    testing put method

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,60 +1,72 @@
 #!/usr/bin/python3
-"""creates a new view for User objects"""
+"""index file"""
+
 from api.v1.views import app_views
-from models.user import User
+from flask import jsonify, abort
 from models import storage
-from flask import jsonify, abort, request
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import hashlib
+from flask import request
 
 
-@app_views.route('/users', methods=["GET"], strict_slashes=False)
-def user_list():
-    """retrieves all user objects"""
-    user_list = []
-    userstorage = storage.all(User).values()
-    for userobj in userstorage:
-        user_list.append(userobj.to_dict())
-    return jsonify(user_list)
+@app_views.route("/users", methods=['GET'], strict_slashes=False)
+def get_users():
+    """All users"""
+    states_dict = []
+    for item in storage.all('User').values():
+        states_dict.append(item.to_dict())
+    return jsonify(states_dict)
 
 
-@app_views.route('/users/<user_id>', methods=["GET"], strict_slashes=False)
-def user_id(user_id):
-    """retrieves a user object"""
+@app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
+def get_user(user_id=None):
+    """User"""
+    if storage.get('User', user_id) is None:
+        abort(404)
+    else:
+        return jsonify(storage.get('User', user_id).to_dict())
+
+
+@app_views.route("/users/<user_id>",
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def del_user(user_id=None):
+    """state"""
+    willy = storage.get('User', user_id)
+    if willy is None:
+        abort(404)
+    else:
+        storage.delete(willy)
+        storage.save()
+        return jsonify({}), 200
+
+
+@app_views.route("/users", methods=['POST'], strict_slashes=False)
+def post_user():
+    """state"""
     try:
-        userobj = storage.get(User, user_id).to_dict()
+        willy = request.get_json()
     except:
-        abort(404)
-    if userobj is None:
-        abort(404)
-    return jsonify(userobj)
-
-
-@app_views.route('/users/<user_id>', methods=["DELETE"], strict_slashes=False)
-def user_delete(user_id=None):
-    """deletes a user object"""
-    userobj = storage.get(User, user_id)
-    if userobj is None:
-        abort(404)
-    storage.delete(userobj)
-    userobj.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/users', methods=["POST"], strict_slashes=False)
-def user_create():
-    """creates a user object"""
-    try:
-        body_dict = request.get_json()
-        if body_dict is None:
-            abort(400, "Not a JSON")
-    except Exception:
-        abort(400, "Not a JSON")
-    if "email" not in body_dict.keys():
-        abort(400, "Missing email")
-    if "password" not in body_dict.keys():
-        abort(400, "Missing password")
-    userobj = User(**body_dict)
-    userobj.save()
-    return jsonify(userobj.to_dict()), 201
+        abort(400, 'Not a JSON')
+    if willy is None:
+        abort(400, 'Not a JSON')
+    elif "email" not in willy.keys():
+        abort(400, 'Missing email')
+    elif "password" not in willy.keys():
+        abort(400, 'Missing password')
+    else:
+        a = willy["password"]
+        a = a.encode('utf-8')
+        test = hashlib.md5(a).hexdigest()
+        new_user = User(email=willy['email'],
+                        password=test)
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=["PUT"],
